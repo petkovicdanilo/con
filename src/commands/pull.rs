1004acf64c00046b2479a23a18699738570d283b
Danilo <petkovicdanilo97@gmail.com>
2021-10-17 19:14:59 +0200

    Fix layer unpacking

--- src/commands/pull.rs
+++ src/commands/pull.rs
@@ -1,14 +1,14 @@
+use std::{fs::File, io::BufReader};
+
 use anyhow::Result;
-use async_compression::tokio::bufread::GzipDecoder;
 use clap::Clap;
+use flate2::bufread::GzDecoder;
 use futures::future::join_all;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use oci_registry::registry::Registry;
 use oci_spec::image::{Arch, Os};
-use tokio::{
-    fs::{create_dir_all, remove_file, rename, File},
-    io::BufReader,
-};
-use tokio_tar::Archive;
+use tar::Archive;
+use tokio::fs::{create_dir_all, remove_file, rename};
 
 use crate::image::{parse_image_id, Image, ImageId};
 
@@ -43,21 +43,38 @@ impl Pull {
 
                 let image = Image::new(self.image_id.name, self.image_id.tag, destination_dir)?;
 
+                let multi_progress = MultiProgress::new();
                 let mut tasks = vec![];
 
                 for layer_path in image.layer_paths() {
+                    let progress_bar = multi_progress.add(ProgressBar::new(0));
+
                     tasks.push(tokio::spawn(async move {
-                        let tar_gz = File::open(&layer_path).await?;
-                        let tar = GzipDecoder::new(BufReader::new(tar_gz));
+                        let tar_gz = File::open(&layer_path)?;
+                        let buf_reader = BufReader::new(tar_gz);
+                        let tar = GzDecoder::new(buf_reader);
                         let mut archive = Archive::new(tar);
 
                         let digest = layer_path.file_name().unwrap().to_str().unwrap();
+                        let alg = layer_path
+                            .parent()
+                            .unwrap()
+                            .components()
+                            .last()
+                            .unwrap()
+                            .as_os_str()
+                            .to_str()
+                            .unwrap();
                         let unpacked_path = layer_path
                             .parent()
                             .unwrap()
                             .join(format!("{}-unpacked", digest));
+
+                        progress_bar.set_style(ProgressStyle::default_bar().template("{msg}"));
+                        progress_bar.set_message(format!("[ ] Unpacking {}:{}", alg, digest));
+
                         create_dir_all(&unpacked_path).await?;
-                        archive.unpack(&unpacked_path).await?;
+                        archive.unpack(&unpacked_path)?;
 
                         remove_file(&layer_path).await?;
                         rename(
@@ -66,10 +83,15 @@ impl Pull {
                         )
                         .await?;
 
+                        progress_bar
+                            .finish_with_message(format!("[x] Unpacked  {}:{}", alg, digest));
                         anyhow::Result::<()>::Ok(())
                     }));
                 }
+
+                let handle_m = tokio::task::spawn_blocking(move || multi_progress.join().unwrap());
                 join_all(tasks).await;
+                handle_m.await.unwrap();
 
                 Ok::<(), anyhow::Error>(())
             })?;
